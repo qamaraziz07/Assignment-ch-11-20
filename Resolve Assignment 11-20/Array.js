bfabca38c9201a33b01f13cc109c787522cf0e05
Qamar Aziz <59906689+qamaraziz07@users.noreply.github.com>
2021-12-07 21:01:23 +0500

    Add files via upload
    Resolved assignmnts

---
+++ Resolve Assignment 11-20/Array.js
@@ -0,0 +1,84 @@
+// var array = [];              //Q:1
+
+// var objArray = [{}];         //Q:2
+
+// var strArray =["CS","SE","IT"];               //Q:3
+//       document.write(strArray + "<br>");
+// var numArray =[123,456,789,00];                //Q:4
+//      document.write(numArray + "<br>");
+// var boolArray = [true,false];                 //Q:5
+//      document.write(boolArray + "<br>");
+// var mixArray = [1,"IT",true,"Karachi"];      //Q:6
+//       document.write(mixArray + "<br>");
+
+// var eduDetails = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];     //Q:7
+//     for(var i=0; i< eduDetails.length; i++)
+//         {
+//             document.write( i + 1 + ")" +eduDetails[i] + "<br>");
+//         };
+
+
+// var students = ["Michael","John","Tony"];            // Q:8
+// var obtScore   =["320","230","480"];
+// var totalMarks = 500;
+// for(var i=0; i< students.length; i++){
+    
+//     var percnt = (obtScore[i] / totalMarks) * 100;
+//     document.write("Score of  " + students[i] + "  is  " +obtScore[i] + ",  Percentage  is : " + percnt +"%"+"<br>");
+// };
+
+
+
+
+// var clrNames =[" RED "," BLACK "," GREEN "," YELLOW " ];     //Q: 9
+// document.write("Main Array :"+clrNames + "<br>");
+
+// var clrAddStrt =prompt("What Colour you want to ADD in the Begining??");
+// clrNames.splice(0 , 1 ,clrAddStrt);
+// document.write("Updated Array :" + clrNames +"<br>");
+
+// var clrAddLst =prompt("What Colour you want to ADD in the Last??");
+// clrNames.push(clrAddLst);
+// document.write("Updated Array :" + clrNames +"<br>");
+
+
+// var clrRemvStrt =prompt("What Colour you want to Remove in the Begining??");
+// clrNames.shift(clrRemvStrt);
+// document.write("Updated Array :" + clrNames +"<br>");
+
+// var clrRemvLst =prompt("What Colour you want to Remove in the Last??");
+// clrNames.pop(clrRemvLst);
+// document.write("Updated Array :" + clrNames +"<br>");
+
+
+
+// var stdScore =[320,230,480,120];        //Q:10
+// document.write("Student's Score :" + stdScore + "<br>");
+// document.write("Ordered Scores of a Students :" + stdScore.sort()+ "<br>");
+
+
+// var cities =[" Lahore"," Karachi"," Islamabad"," Quetta"," Peshawar."];   //Q:11
+//  document.write("Cities List :" + "<br>" + cities + "<br>");
+//  var selectedCities = cities.slice(2,4);
+// document.write("Selected Cities List :"  + "<br>" + selectedCities + "<br>" );
+
+
+//  var arr =["This","is","my","cat"];      //Q:12
+//  var newArr = arr.join(" ");
+//  document.write("Array : " + "<br>" + arr + "<br>");
+//  document.write("String :" + "<br>" + newArr +"<br>");
+
+// var devices =[" Keyboard "," Mouse "," Printer "," Monitor "];   //Q:13
+// for (var i=0; i<=devices.length; i++){
+// var otherdev = devices.shift();
+// document.write(" OUT :" +"<br>"+ otherdev +"<br>");
+// }; 
+
+// var newDevices =[" Keyboard "," Mouse "," Printer "," Monitor "]; //Q:14
+// for(var i=0; i<=newDevices.length; i++){
+//     var otherdev = newDevices.pop();
+//     document.write(" OUT :" +"<br>"+ otherdev +"<br>");
+// };
+
+//// Question No 15 ,I don't understand method that was using in it...
+
---
+++ Resolve Assignment 11-20/loops.js
@@ -0,0 +1,172 @@
+///////////////////////////////////////////////////////////////////////////////////////
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck =prompt("check city??");
+
+// if ( cityToCheck === cleanestCities[0]) {
+//     alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[1]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[2]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[3]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[4]) {
+//     alert("It's one of the cleanest cities");
+//      }
+    
+//     else {
+//      alert("It's not on the list");
+//     };
+
+/////////////////////////////////////////////////////////////////////////////
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i =0; i<firstNames.length; i++) {
+//  for (var j =0; j<lastNames.length; j++) {
+//   var foo =fullNames.push(firstNames[i] + lastNames[j] + "<br>") ;
+//  }
+//  console.log(foo);
+//  document.write(fullNames + "<br>");
+// };
+///////////////////////////////////////////////////////////////////////////
+
+
+//<<<Q:1>>
+// var activites=[];
+// var activites =[
+//     ["work",9],
+//     ["Eat",8],
+//     ["play games", 7],
+
+// ];
+// console.table(activites);
+
+//<<**Q:2>>
+
+// const myMatrix = [];
+// const rows = 5;
+// const columns = 3;
+
+// for (var i = 0; i < rows; i++) {
+//   myMatrix[i] = [];
+//   for (var j = 0; j < columns; j++) {    
+//     myMatrix[i][j] = (i*columns)+j+1;
+//   }
+// }
+// console.log(myMatrix);
+
+
+//<<Q:3>>
+// var num=10;
+// for(i=1; i<=num; i++){
+// document.write(i + "<br>");
+// };
+
+//<<Q:4>>
+// var user =+prompt("Enter No as you see Table.. ");
+// var range= prompt("Enter a length of Table..");
+// for (var i=1; i<=range; i++){
+//     document.write(user + "*" + i + " =" + user * i +"<br>");
+// };
+
+//<<Q:5>>
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+// for(var i=0; i< fruits.length; i++){
+
+//     document.write(fruits[i] + "<br>");
+//     document.write("Element at index" + i +"is "+ fruits[i] +"<br>")
+// };
+
+
+///<<Q:6>>>
+
+// var num=15;
+// document.write("Counting :" +"<br>");
+// for(i=1; i<=num; i++){
+//     document.write( i+"," );
+// };
+
+        // var num=10;
+        // document.write("Reverse Counting :" + "<br>");
+        // for(var i=10; i>=0; i--){
+        //    document.write( i+"," );
+        // };
+
+// var num=20;
+// document.write("Even :" + "<br>");
+// for(var i=0; i<=num; i=i+2){
+//    document.write( i+"," );
+// };
+
+        // var num=20;
+        // document.write("Odd :" + "<br>");
+        // for(var i=1; i<=num; i=i+2){
+        //    document.write( i+"," );
+        // };
+
+            // var num=20;
+            // document.write("Series :" + "<br>");
+            //  for(var i=2; i<=num; i=i+2){
+            //     document.write( i+"k"+"," );
+            // };   
+
+
+/////<<<Q:7>>
+// var items=["cake","apple pie","chips","cookie","patties"];
+// var itemToCheck =prompt("Welcome to ABC Bakery.What do you want to order Sir/Mam?");
+// for(var i=0; i<=items.length; i++){
+// if(itemToCheck === items[i]){
+//     document.write("Cake is availble at index "+ i + "in our Bakery");
+// }
+// };
+
+////<<Q:8>>>
+// var num = [24, 53, 78, 91, 12]
+// var largest =0;
+// for(i=0; i<=largest; i++){
+// if(num[i]>largest){
+// var largest=num[i];
+// }
+// };
+// document.write("Array items :"+ num +"<br>");
+// document.write("The largest number is "+largest);
+
+
+////<<Q:9>>
+// var num = [24, 53, 78, 91, 12]
+// var lowest = num[0];
+// for(i=0; i<=lowest; i++){
+// if(num[i]<lowest){
+// var lowest=num[i];
+// }
+// };
+// document.write("Array items :"+ num +"<br>");
+// document.write("The Smallest number is "+lowest);
+
+////<<Q:10>>
+
+// var num =5;
+// var range= 20;
+// for (var i=1; i<=range; i++){
+//     document.write(num * i +" , " );
+// };
+
+
+///<<Q:11>>>
+// var num=20;
+// for(i=0; i<=num; i++){
+//     if(i % 2 == 0){
+//         document.write(i +" is even "+"<br>" );
+//     }
+//     else if (i % 2 !== 0){
+//         document.write( i +" is Odd "+"<br>" );
+//     };
+// };
+
+
+
